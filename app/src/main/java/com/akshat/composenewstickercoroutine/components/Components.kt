dfaf2df3cf6b10203a759444a8600908e4c6ac28
Akshat <akshat@Akshats-MacBook-Pro.local>
2024-10-10 14:21:40 -0400

    Adding Ui on top

--- app/src/main/java/com/akshat/composenewstickercoroutine/components/Components.kt
+++ app/src/main/java/com/akshat/composenewstickercoroutine/components/Components.kt
@@ -1,32 +1,44 @@
 package com.akshat.composenewstickercoroutine.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
+import com.akshat.composenewstickercoroutine.model.NewsArticle
 import com.akshat.composenewstickercoroutine.utils.Constants
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -65,4 +77,103 @@ fun NewsTickerAppBar(
             containerColor = Constants.APP_BAR_THEME_COLOR),
     )
 
+}
+
+@Composable
+fun NewsArticleRow(
+    newsArticle: NewsArticle
+) {
+
+    Card(
+        modifier = Modifier
+            .padding(top = 6.dp)
+            .fillMaxWidth()
+            .height(350.dp)
+            .wrapContentHeight()
+            .clickable {},
+        shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+        colors = CardColors(
+            Color.White,
+            Color.Black,
+            Color.White,
+            Color.White
+        ),
+        elevation = CardDefaults.cardElevation(6.dp)
+
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+
+
+            newsArticle.urlToImage?.let { NewsImage(imageUrl = it, newsArticle.author.toString()) }
+
+
+            NewsDetails(newsArticle = newsArticle)
+        }
+    }
+
+
+}
+
+@Composable
+fun NewsDetails(newsArticle: NewsArticle) {
+    Column(
+        modifier = Modifier
+            .height(IntrinsicSize.Min)
+            .padding(start = 8.dp)
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.Start
+    ) {
+
+        Row(modifier = Modifier.padding(4.dp)) {
+            Text(
+                text = newsArticle.title.toString(),
+                modifier = Modifier.padding(end = 10.dp),
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+
+        Row(modifier = Modifier.padding(8.dp)) {
+            Text(
+                text = newsArticle.publishedAt.toString(),
+                modifier = Modifier.padding(end = 10.dp),
+                color = Color.Blue,
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+    }
+
+}
+
+@Composable
+fun NewsImage(imageUrl: String, title: String) {
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp)
+            .background(Color(0f, 0f, 1f, 0.5f)),
+        contentAlignment = Alignment.BottomStart
+    ) {
+
+        Image(
+            painter = rememberAsyncImagePainter(imageUrl),
+            contentDescription = "news image",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(250.dp),
+            contentScale = ContentScale.FillBounds
+        )
+
+        Text(
+            text = title,
+            modifier = Modifier.padding(start = 6.dp),
+            color = Color.White,
+            style = MaterialTheme.typography.headlineMedium
+        )
+    }
 }
--- app/src/main/java/com/akshat/composenewstickercoroutine/screens/NewsTickerHomeScreen.kt
+++ app/src/main/java/com/akshat/composenewstickercoroutine/screens/NewsTickerHomeScreen.kt
@@ -1,21 +1,28 @@
 package com.akshat.composenewstickercoroutine.screens
 
+import android.util.Log
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Observer
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.navigation.NavHostController
+import com.akshat.composenewstickercoroutine.components.NewsArticleRow
 import com.akshat.composenewstickercoroutine.components.NewsTickerAppBar
+import com.akshat.composenewstickercoroutine.model.NewsArticle
+import kotlinx.coroutines.flow.MutableStateFlow
 
 @Composable
 fun NewsTickerHomeScreen(navController: NavHostController, viewModel: HomeScreenViewModel) {
@@ -29,16 +36,43 @@ fun NewsTickerHomeScreen(navController: NavHostController, viewModel: HomeScreen
                 .fillMaxSize()
         ) {
             val lifecycleOwner = LocalLifecycleOwner.current
-            var text by remember {
-                mutableStateOf("")
-            }
+
+            var _newsList = remember { MutableStateFlow(emptyList<NewsArticle>()) }
+            val newsList by remember { _newsList }.collectAsState()
+            val newList : ArrayList<NewsArticle> = ArrayList()
 
            viewModel.newsArticles.observe( lifecycleOwner, Observer { article ->
-               text = article.toString()
+               newList.add(0, article)
+               _newsList.value += article
+               Log.d("TRACK", "response is ${_newsList.value}")
            })
 
-            Text("Text is $text")
+            MainContent(newsList)
 
         }
     }
 }
+
+
+@Composable
+fun MainContent(text: List<NewsArticle>) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        color = Color(0xFFEEF1EF)
+    ) {
+        LazyColumn(
+            modifier = Modifier.padding(2.dp),
+            contentPadding = PaddingValues(1.dp)
+        ) {
+
+            items(text.size) { it ->
+                NewsArticleRow(text[it])
+            }
+
+        }
+
+    }
+}
+
